51420e0796a85732177a7c3b6640678d461181a6
ishak-devs <kouicicontact@yahoo.com>
2025-11-14 12:18:17 +0100

    fix midl

--- layout/components/buttons/backbutton.py
+++ layout/components/buttons/backbutton.py
@@ -1,8 +1,7 @@
 from PyQt6.QtWidgets import QPushButton
 from PyQt6.QtCore import Qt
 
-def backbutton(layout, navigate_home=None):
-    back_btn = QPushButton("← Retour")
+def backbutton(navigate_home=None):
+    back_btn = QPushButton("<- Retour")
     if navigate_home:
         back_btn.clicked.connect(navigate_home)
-    layout.addWidget(back_btn, alignment=Qt.AlignmentFlag.AlignLeft)
--- layout/form/form_cv.py
+++ layout/form/form_cv.py
@@ -1,10 +1,9 @@
-from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QCheckBox, QHBoxLayout, QRadioButton, QButtonGroup
+from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QCheckBox, QHBoxLayout, QRadioButton, QButtonGroup
 from PyQt6.QtCore import Qt
 from treatment.upload_cv import upload_cv
 from treatment.fill_template import fill_template
 from layout.components.buttons.backbutton import backbutton
-from layout.components.header.logo import LogoWidget
-from treatment.treatment_cv_form import Treatment_cv_form
+from layout.components.images.logo import LogoWidget
 
 class CVUploadForm(QWidget):
     def __init__(self, navigate_home=None):
@@ -13,47 +12,46 @@ class CVUploadForm(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        layout = QVBoxLayout(self)
-        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.setSpacing(15)
+        main_layout = QVBoxLayout(self)
 
         logo_widget = LogoWidget()
-        layout.addWidget(logo_widget, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+        main_layout.addWidget(logo_widget, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
 
-        backbutton(layout, self.navigate_home)
+        main_layout.addStretch()
 
-        self.file_label = QLabel("")
-        layout.addWidget(self.file_label, alignment=Qt.AlignmentFlag.AlignCenter)
+        center_layout = QVBoxLayout()
+        center_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)  
 
         self.upload_btn = QPushButton("Sélectionner un cv")
         self.upload_btn.setEnabled(True)
         self.upload_btn.clicked.connect(lambda: upload_cv(self))
-        layout.addWidget(self.upload_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        center_layout.addWidget(self.upload_btn)
 
         cv_type_layout = QHBoxLayout()
-        
+
         self.cv_simple = QRadioButton("CV simple", self)
-        self.cv_simple.setEnabled(False)
-        
         self.cv_complex = QRadioButton("CV complex", self)
+        
+        self.english_cv = QCheckBox("CV anglais", self)
+        self.cv_simple.setEnabled(False)
         self.cv_complex.setEnabled(False)
+        self.english_cv.setEnabled(False)
+        self.english_cv.setChecked(False)
 
         self.cv_type_group = QButtonGroup(self)
         self.cv_type_group.addButton(self.cv_simple)
         self.cv_type_group.addButton(self.cv_complex)
-
-        self.english_cv = QCheckBox("CV anglais", self)
-        self.english_cv.setEnabled(False)
-        self.english_cv.setChecked(False)
 
         cv_type_layout.addStretch()
         cv_type_layout.addWidget(self.cv_simple)
         cv_type_layout.addWidget(self.cv_complex)
         cv_type_layout.addWidget(self.english_cv)
         cv_type_layout.addStretch()
+        center_layout.addLayout(cv_type_layout)
 
-        layout.addLayout(cv_type_layout)
-
-        self.valider_btn = QPushButton("Valider")
+        self.valider_btn = QPushButton("Générer un dossier")
         self.valider_btn.clicked.connect(lambda: fill_template(self))
-        layout.addWidget(self.valider_btn)
+        center_layout.addWidget(self.valider_btn)
+        main_layout.addLayout(center_layout)
+
+        main_layout.addStretch()

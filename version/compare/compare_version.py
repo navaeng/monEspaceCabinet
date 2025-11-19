d823bda9803c5cbc066b6264b350bd76e20bac3a
ishak-devs <kouicicontact@yahoo.com>
2025-11-19 15:04:47 +0100

    v1.1.2

--- version/compare/compare_version.py
+++ version/compare/compare_version.py
@@ -18,7 +18,7 @@ def compare_version():
     print(response.status_code)
     latest_version = response.json()["tag_name"]
     print('remote_version:', remote_version)
-    current_version = "v1.1.1"
+    current_version = "v1.1.2"
 
     if latest_version > current_version:
         

6457a431cfe104cf6f88c55469e958dc26dbb8f3
Ishak <i.kouici@insta.fr>
2026-01-30 21:39:09 +0100

    lancement ok mais il faut preparer un lancement aleatoire et un a la fois

--- front/src/supabaseClient.js
+++ front/src/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://czvodiqotcyurmkwgahs.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6dm9kaXFvdGN5dXJta3dnYWhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1NTY1MzksImV4cCI6MjA4MjEzMjUzOX0.iWg9O2W813Ks5ncPZ0ecea5LpJP3RIJe6PgEs_Fp6nQ";
 
+console.log("URL:", supabaseUrl);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

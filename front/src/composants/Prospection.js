93b8e20cc9c282cb1fb36018a5515423e3207648
ishak <i.kouici@insta.fr>
2026-02-05 15:00:28 +0100

    translate binary element with mammoth

--- front/src/composants/Prospection.js
+++ front/src/composants/Prospection.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../supabaseClient";
-
+import mammoth from 'mammoth';
 function Prospection() {
   const [intitule, setIntitule] = useState("");
 
@@ -23,17 +23,21 @@ function Prospection() {
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
-    const reader = new FileReader();
+    if (file.type === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
 
-    reader.onload = (event) => {
-      const text = event.target.result;
-      console.log("Texte lu :", text.substring(0, 50));
-      setOffre(text);
-    };
+    const reader = new FileReader();
+      reader.onload = async (event) => {
+        if (file.name.endsWith("docx")) {
+        const {value} = await mammoth.extractRawText({arrayBuffer: event.target.result});
+        setOffre(value);
+      }
+    }
     if (file) {
       reader.readAsText(file);
     }
-  };
+    };
+
+    file.name.endsWith("docx") ? reader.readAsArrayBuffer(file) : reader.readAsText(file);
 
   const FetchProspection = async () => {
     try {

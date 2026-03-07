4e3d3df30994ce6b70ee6e10d03fce2c4c2e5736
Ishak <i.kouici@insta.fr>
2026-03-07 19:39:49 +0100

    changement de render document

--- system/serveur/USECASE/dossier_competences/services/render/render_document.py
+++ system/serveur/USECASE/dossier_competences/services/render/render_document.py
@@ -1,15 +1,5 @@
-import os
-from docxtpl import DocxTemplate
-
-from USECASE.dossier_competences.jinja2.create_jinra_env import create_jinra_env
-
+from docx import Document
 
 def render_document(data, output_path):
-
-    template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..", "ressources", "template_4.docx")
-
-    doc = DocxTemplate(template_path)
-    create_jinra_env(doc)
-
-    doc.render({"data": data})
+    doc = Document()
     doc.save(output_path)

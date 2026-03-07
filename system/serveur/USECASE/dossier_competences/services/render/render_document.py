4b4a8f684c7f5e3e1324f8159a7262174805e188
Ishak <i.kouici@insta.fr>
2026-03-07 19:43:32 +0100

    fix

--- system/serveur/USECASE/dossier_competences/services/render/render_document.py
+++ system/serveur/USECASE/dossier_competences/services/render/render_document.py
@@ -1,5 +1,9 @@
 from docx import Document
 
+from USECASE.dossier_competences.library.python_docx.header.header_doc import header_doc
+
+
 def render_document(data, output_path):
     doc = Document()
+    header_doc(doc, data, output_path)
     doc.save(output_path)

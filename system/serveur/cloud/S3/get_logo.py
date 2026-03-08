9b2d191061e23065865cdb1202d2954ab7811bec
Ishak <i.kouici@insta.fr>
2026-03-08 13:47:55 +0100

    ecriture dans logo pour recuperer depuis le cloud

--- system/serveur/cloud/S3/get_logo.py
+++ system/serveur/cloud/S3/get_logo.py
@@ -11,11 +11,22 @@ def get_logo():
         's3',
         endpoint_url=os.getenv('S3_ENDPOINT'),
         aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
-        aws_secret_access_key=os.getenv('S3_SECRET_KEY')
+        aws_secret_access_key=os.getenv('S3_SECRET_KEY'),
+        region_name="us-west-1"
     )
+    print(s3_client.list_buckets()['Buckets'])
     print(f"DEBUG KEY: {os.getenv('S3_ACCESS_KEY')}")
     print(f"DEBUG KEY: {os.getenv('S3_SECRET_KEY')}")
     print(f"DEBUG KEY: {os.getenv('S3_ENDPOINT')}")
 
-    response = s3_client.get_object(Bucket='mybuckets', Key='logo.jpg')
-    return BytesIO(response['Body'].read())
+    response = s3_client.get_object(Bucket='mybucket', Key='logoNAVA.jpg')
+    #
+    # img_data = BytesIO(response['Body'].read())
+    # img_data.seek(0)
+    # return img_data
+
+    content = response['Body'].read()
+    print(f"DEBUG IMAGE: {len(content)} bytes, starts with: {content[:10]}")
+    img_data = BytesIO(content)
+    img_data.seek(0)
+    return img_data

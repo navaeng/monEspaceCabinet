3eadf135cc1e298e6ac7854f720cb02d742a35ba
ishak <i.kouici@insta.fr>
2026-03-10 14:31:50 +0100

    changement du prompt

--- system/serveur/usecase/dossier_competences/IA/prompt/main/main_prompt.py
+++ system/serveur/usecase/dossier_competences/IA/prompt/main/main_prompt.py
@@ -2,7 +2,7 @@ def main_prompt(cv_text, current_template):
 
     return f"""Tu es un expert en extraction de données. ton but est de structurer dans le json : {current_template} 
     tout le contenu dans son intégralité sans commentaires ,
-    pour les champ contenant 'à définir', tu dois générer toi-même une valeur
+    si une clé contient 'à définir', tu dois générer toi-même une valeur
 DATA:
 {cv_text}
 """

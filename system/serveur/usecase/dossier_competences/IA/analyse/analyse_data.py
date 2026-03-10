374d95f44262493702e691f626d7381a01d61b7f
ishak <i.kouici@insta.fr>
2026-03-10 09:20:57 +0100

    utilisation de repair json pour transforme une chaîne corrompue en JSON valide

--- system/serveur/usecase/dossier_competences/IA/analyse/analyse_data.py
+++ system/serveur/usecase/dossier_competences/IA/analyse/analyse_data.py
@@ -1,28 +1,4 @@
-# import ast
-# import re
-#
-# from usecase.dossier_competences.IA.prompt.main.main_prompt import main_prompt
-# from usecase.dossier_competences.services.data.reading.read_cv import read_cv
-# from services.api_externes.openrouter.call_openrouter import call_openrouter
-# import json
-#
-# def analyse_data(file_path):
-#     cv_text = read_cv(file_path)
-#     if not cv_text:
-#         print("Erreur : Impossible de lire le texte du CV")
-#         return {}
-#
-#     output = call_openrouter(main_prompt(cv_text), model="nousresearch/hermes-4-405b", json_mode=True)
-#
-#     json_match = re.search(r'\{.*\}', output, re.DOTALL)
-#     if json_match:
-#         output = json_match.group(0)
-#
-#     try:
-#         print(output)
-#         return json.loads(output)
-#     except json.JSONDecodeError:
-#         return ast.literal_eval(output)
+from json_repair import repair_json
 import json
 
 from services.api_externes.openrouter.call_openrouter import call_openrouter
@@ -33,17 +9,13 @@ from usecase.dossier_competences.services.data.reading.read_cv import read_cv
 def analyse_data(file_path):
     cv_text = read_cv(file_path)
     output = call_openrouter(main_prompt(cv_text), model="nousresearch/hermes-4-405b", json_mode=True)
+    print(output)
 
-    output = output.replace('```json', '').replace('```', '').strip()
+    # transforme une chaîne corrompue en JSON valide
+    repaired_output = repair_json(output)
 
     try:
-        return json.loads(output)
-    except (json.JSONDecodeError, SyntaxError):
-        try:
-
-            if output.count('[') > output.count(']'): output += ']' * (output.count('[') - output.count(']'))
-            if output.count('{') > output.count('}'): output += '}' * (output.count('{') - output.count('}'))
-            return json.loads(output)
-        except:
-            print("Le JSON est trop corrompu ou tronqué pour être réparé.")
-            return {}
+        return json.loads(repaired_output)
+    except:
+        print("Échec critique du parsing malgré la réparation.")
+        return {}

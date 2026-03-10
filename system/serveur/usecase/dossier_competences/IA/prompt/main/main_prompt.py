2ef96b04ca70aaa3cf681796e8afed41791d08e4
ishak <i.kouici@insta.fr>
2026-03-10 15:20:42 +0100

    retirer le laisse vide dans le prompt

--- system/serveur/usecase/dossier_competences/IA/prompt/main/main_prompt.py
+++ system/serveur/usecase/dossier_competences/IA/prompt/main/main_prompt.py
@@ -2,5 +2,5 @@ def main_prompt(cv_text, current_template):
 
     return f"""Tu es un expert en extraction de données. Consigne strict : ton but est d'extraire tout le contenu de {cv_text} dans le json : {current_template} 
     dans son intégralité sans commentaires.
-    Si une clé contient 'à définir', tu dois générer toi-même une valeur, pour les valeurs non trouvé laisse vide.
+    Si une clé contient 'à définir', tu dois générer toi-même une valeur.
 """

497139f54f65bbdfb4bb8ebdaadfa9541e4a776c
ishak <i.kouici@insta.fr>
2026-03-11 12:29:36 +0100

    fix get user id ajout de ()

--- system/serveur/usecase/linkedin/query/user/get_user_id.py
+++ system/serveur/usecase/linkedin/query/user/get_user_id.py
@@ -14,7 +14,7 @@ async def get_user_id(request: Request):
 
     token = auth_header.replace("Bearer ", "")
 
-    user_response = supabase_client.auth.get_user(token)
+    user_response = supabase_client().auth.get_user(token)
     if not user_response or not user_response.user:
         raise HTTPException(status_code=401, detail="Authentification invalide")
     current_user_id = user_response.user.id

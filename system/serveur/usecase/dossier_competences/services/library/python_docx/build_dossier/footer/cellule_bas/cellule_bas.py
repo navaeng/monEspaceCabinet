5853b96ad3f45f5b3044768b759bfae8594d9c18
ishak <i.kouici@insta.fr>
2026-03-13 10:04:40 +0100

    ajout des elements dans le footer

--- system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/footer/cellule_bas/cellule_bas.py
+++ system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/footer/cellule_bas/cellule_bas.py
@@ -1,10 +1,15 @@
 from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.shared import Pt, RGBColor
+from docx.shared import RGBColor, Pt
 
 
 def cellule_bas(tab):
     p2 = tab.cell(1, 0).paragraphs[0]
     p2.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    r = p2.add_run(
-        "NAVA ENGINEERING • 9-11 AVENUE MICHELET, 93400 SAINT-OUEN-SUR-SEINE\nSimon ZANA • 06 13 53 23 81 • simon.zana@nava-eng.com")
-    r.font.size, r.font.color.rgb = Pt(9), RGBColor(0, 0, 20)
+    r1 = p2.add_run("NAVA ENGINEERING • 9-11 AVENUE MICHELET, 93400 SAINT-OUEN-SUR-SEINE\n"
+                    "Simon ZANA • 06 13 53 23 81 • ")
+    r1.font.size, r1.font.color.rgb = Pt(9), RGBColor(0, 32, 96)
+
+    r2 = p2.add_run("simon.zana@nava-eng.com")
+    r2.font.size, r2.font.color.rgb, r2.font.underline = Pt(8), RGBColor(0, 32, 96), True
+
+
--- system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/footer/cellule_haut/cellule_haut.py
+++ system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/footer/cellule_haut/cellule_haut.py
@@ -3,9 +3,19 @@ from docx.oxml import parse_xml
 from docx.oxml.ns import nsdecls
 
 
-def cellule_haut(tab):
+# def cellule_haut(tab):
+#     p1 = tab.cell(0, 0).paragraphs[0]
+#     p1.alignment = WD_ALIGN_PARAGRAPH.CENTER
+#     p1._p.append(parse_xml(
+#         f'<w:sdt {nsdecls("w")}><w:sdtPr><w:shd w:fill="002060"/></w:sdtPr><w:sdtContent><w:p><w:pPr><w:jc '
+#         f'w:val="center"/></w:pPr><w:r><w:rPr><w:color w:val="FFFFFF"/></w:rPr><w:fldChar '
+#         f'w:fldCharType="begin"/><w:instrText>PAGE</w:instrText><w:fldChar '
+#         f'w:fldCharType="end"/></w:r></w:p></w:sdtContent></w:sdt>'))
 
-    p1 = tab.cell(0, 0).paragraphs[0]
-    p1.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    p1._p.append(parse_xml(
-        f'<w:sdt {nsdecls("w")}><w:sdtPr><w:shd w:fill="0000FF"/></w:sdtPr><w:sdtContent><w:p><w:pPr><w:jc w:val="center"/></w:pPr><w:r><w:rPr><w:color w:val="FFFFFF"/></w:rPr><w:fldChar w:fldCharType="begin"/><w:instrText>PAGE</w:instrText><w:fldChar w:fldCharType="end"/></w:r></w:p></w:sdtContent></w:sdt>'))
+def cellule_haut(tab):
+        p1 = tab.cell(0, 0).paragraphs[0]
+        p1.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        p1._p.append(parse_xml(
+            f'<w:r {nsdecls("w")}><w:rPr><w:shd w:fill="002060"/><w:color w:val="FFFFFF"/></w:rPr>'
+            f'<w:fldChar w:fldCharType="begin"/><w:instrText>PAGE</w:instrText><w:fldChar w:fldCharType="end"/></w:r>'
+        ))

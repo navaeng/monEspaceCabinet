8e1b47e7764b5a9726d702a4fdef59fc817d856d
ishak <i.kouici@insta.fr>
2026-03-16 09:56:02 +0100

    delete yield

--- system/serveur/usecase/linkedin/services/selenium/actions/write_and_send_message.py
+++ system/serveur/usecase/linkedin/services/selenium/actions/write_and_send_message.py
@@ -7,8 +7,8 @@ def write_and_send_message(driver, btn_element):
         "arguments[0].removeAttribute('disabled');", btn_element
     )
     time.sleep(random.uniform(7, 9))
-    yield "Envoi du message..."
+
     driver.execute_script("arguments[0].click();", btn_element)
     time.sleep(random.uniform(6, 9))
-    yield "✅ Message envoyé !"
+
     time.sleep(random.uniform(5, 9))

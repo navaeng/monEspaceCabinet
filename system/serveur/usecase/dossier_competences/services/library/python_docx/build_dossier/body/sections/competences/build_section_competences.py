727ea0a0039e45eed286aa56440fe00f44f5aba5
ishak <i.kouici@insta.fr>
2026-03-19 10:48:39 +0100

    Update system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/sections/competences/build_section_competences.py

--- system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/sections/competences/build_section_competences.py
+++ system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/sections/competences/build_section_competences.py
@@ -1,7 +1,7 @@
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.oxml import parse_xml
 from docx.oxml.ns import nsdecls
-from docx.shared import RGBColor, Pt, Cm
+from docx.shared import Pt, Cm, RGBColor
 
 from usecase.dossier_competences.services.library.python_docx.build_dossier.body.header_section.header_section import \
     header_section
@@ -17,5 +17,9 @@ def build_section_competences(doc, data):
 
     for comp in (data.get('15_Phrases_Percutantes_Compétences') or data.get('Compétences') or []):
         p_item = doc.add_paragraph(comp, style='List Bullet')
-        p_item.paragraph_format.space_after = Pt(2)
-        p_item.paragraph_format.left_indent = Pt(20)
+
+        for run in p_item.runs:
+            run.font.color.rgb = RGBColor(0x00, 0x20, 0x60)
+
+        p_item.paragraph_format.left_indent = Cm(1)
+        p_item.paragraph_format.space_after = Pt(3)

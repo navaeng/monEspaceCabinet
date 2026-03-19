92412df6975e2a875e0de44147dc79af50cd7606
ishak <i.kouici@insta.fr>
2026-03-19 09:35:46 +0100

    Update system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/header/left/header_left.py,system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/header/main/header_doc.py system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/header/right/header_right.py

--- system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/header/right/header_right.py
+++ system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/header/right/header_right.py
@@ -9,24 +9,42 @@ from usecase.dossier_competences.services.library.python_docx.build_dossier.head
 
 POLICE = "Calibri (corps)"
 
-def header_right(doc, data, p):
+def header_right(data: object, cell_droite: object) -> None:
 
-    p.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+    cell_droite.vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+
+    p1 = cell_droite.paragraphs[0]
+    p1.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+    p1.paragraph_format.space_before = Pt(0)
+    p1.paragraph_format.space_after = Pt(0)
+
     initiales = generate_initial(data)
-    run1 = p.add_run(initiales)
+    print(initiales)
+
+    run1 = p1.add_run(initiales)
     run1.font.name = POLICE
-    run1.font.bold = False
+    run1.font.bold = True
     run1.font.size = Pt(17)
     run1.font.color.rgb = RGBColor(0x00, 0x20, 0x60)
 
-    run2 = p.add_run(data.get('Poste_actuel', ''))
+    p2 = cell_droite.add_paragraph()
+    p2.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+    p2.paragraph_format.space_before = Pt(0)
+    p2.paragraph_format.space_after = Pt(0)
+    run2 = p2.add_run(data.get('Poste_actuel', ''))
     run2.font.name = POLICE
-    run2.font.size = Pt(20)
-    run2.font.bold = True
+    run2.font.size = Pt(14)
     run2.font.color.rgb = RGBColor(0x00, 0x20, 0x60)
+    run2.font.bold = True
 
-    run3 = p.add_run(generate_exp(data))
+    p3 = cell_droite.add_paragraph()
+    p3.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+    p3.paragraph_format.space_before = Pt(0)
+    p2.paragraph_format.space_after = Pt(0)
+
+    exp_val = generate_exp(data)
+
+    run3 = p3.add_run(exp_val)
     run3.font.name = POLICE
-    run3.font.bold = False
     run3.font.size = Pt(14)
     run3.font.color.rgb = RGBColor(0x00, 0x20, 0x60)

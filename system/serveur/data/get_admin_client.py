8e2caff2859dcea8224361f097e05a9ebe7649fe
ishak <i.kouici@insta.fr>
2026-03-19 08:58:11 +0100

    Update .idea/workspace.xml,system/serveur/data/get_admin_client.py

--- system/serveur/data/get_admin_client.py
+++ system/serveur/data/get_admin_client.py
@@ -6,12 +6,12 @@ from supabase import create_client
 
 def get_admin_client():
 
-    base_dir = Path(__file__).resolve().parent.parent.parent
-    env_path = base_dir / ".env"
-
-    if not env_path.exists():
-        raise FileNotFoundError(f"Fichier .env introuvable ici : {env_path}")
-    load_dotenv(dotenv_path=env_path)
+    # base_dir = Path(__file__).resolve().parent.parent.parent
+    # env_path = base_dir / ".env"
+    #
+    # if not env_path.exists():
+    #     raise FileNotFoundError(f"Fichier .env introuvable ici : {env_path}")
+    load_dotenv()
 
     url = os.environ.get("SUPABASE_URL", "")
     service_key = os.environ.get("SUPABASE_SERVICE_ROLE_KEY", "")

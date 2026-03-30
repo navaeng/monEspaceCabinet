1c82ca5eeb4f44a9c57705fcf7f11f665eb1ef76
ishak-devs <144032436+Ishak-devs@users.noreply.github.com>
2026-03-30 13:51:58 +0200

    Update competences_template.py

--- system/serveur/usecase/dossier_competences/json/templates/competences/competences_template.py
+++ system/serveur/usecase/dossier_competences/json/templates/competences/competences_template.py
@@ -1,4 +1,4 @@
 def competences_template():
     return {
-        "15_Phrases_Percutantes_Precises_Competences_Candidat_Commencant_Par_Un_Verbe_A_Infinitif": [],
+        "15_Phrases_Percutantes_Competences_Candidat_Commencant_Par_Un_Verbe_A_Infinitif": [],
     }

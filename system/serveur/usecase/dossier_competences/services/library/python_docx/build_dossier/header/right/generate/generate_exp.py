21033281827a791833f088406180b0deb5e8efb3
ishak-devs <144032436+Ishak-devs@users.noreply.github.com>
2026-03-30 14:07:44 +0200

    Update generate_exp.py

--- system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/header/right/generate/generate_exp.py
+++ system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/header/right/generate/generate_exp.py
@@ -1,10 +1,27 @@
+# def generate_exp(data):
+#     try:
+#         val = float(str(data.get('Expérience_Totale_Années', '')).split('an')[0].strip())
+#         ans = int(val)
+#         mois = round((val - ans) * 12)
+
+#         print(val, ans)
+
+#         parts = []
+#         if ans > 0: parts.append(f"{ans} an{'s' if ans > 1 else ''}")
+#         if mois > 0: parts.append(f"{mois} mois")
+
+#         return " et ".join(parts) + " d'expérience" if parts else "Moins d'un an d'expérience"
+
+#     except:
+#         return f"{data.get('Expérience_Totale_Années', '')} d'expérience"
+
 def generate_exp(data):
     try:
-        val = float(str(data.get('Expérience_Totale_Années', '')).split('an')[0].strip())
+        nettoyage = str(data.get('Expérience_Totale_Années', '')).replace('+', '').replace('÷', '').strip()
+        val = float(nettoyage.split('an')[0].strip())
+
         ans = int(val)
         mois = round((val - ans) * 12)
-
-        print(val, ans)
 
         parts = []
         if ans > 0: parts.append(f"{ans} an{'s' if ans > 1 else ''}")

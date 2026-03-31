5f213f964331c687613a72aab54063be5d563a68
ishak-devs <144032436+Ishak-devs@users.noreply.github.com>
2026-03-31 15:38:19 +0200

    Enhance paragraph formatting for languages section
    Added tab stop at 4cm and adjusted run formatting.

--- system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/sections/langues/build_section_langues.py
+++ system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/sections/langues/build_section_langues.py
@@ -1,6 +1,6 @@
 from docx.oxml import parse_xml
 from docx.oxml.ns import nsdecls
-from docx.shared import Pt, RGBColor
+from docx.shared import Pt, RGBColor, Cm  # Ajout de Cm ici
 
 from usecase.dossier_competences.services.library.python_docx.build_dossier.body.header_section.header_section import \
     header_section
@@ -17,11 +17,15 @@ def build_section_langues(doc, data):
     for langue in data.get('Langues', []):
         p = doc.add_paragraph(style='List Bullet')
         p.paragraph_format.keep_together = True
+        
+        # On définit un taquet de tabulation (ex: à 4cm du bord gauche)
+        p.paragraph_format.tab_stops.add_tab_stop(Cm(4))
 
-        run_l = p.add_run(f"{langue.get('Langue')} : ")
+        run_l = p.add_run(langue.get('Langue'))
         run_l.bold = True
-        run_l.font.color.rgb = RGBColor(00, 20, 60)
+        run_l.font.color.rgb = RGBColor(0, 20, 60)
 
-        run_n = p.add_run(langue.get('Niveau'))
-        run_n.font.color.rgb = RGBColor(00, 20, 60)
+        run_n = p.add_run(f"\t{langue.get('Niveau')}")
+        run_n.font.color.rgb = RGBColor(0, 20, 60)
+        
         p.paragraph_format.space_after = Pt(2)

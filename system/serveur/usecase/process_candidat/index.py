560b61bcfd187fcd904bec86846101817dbb9390
ishak <i.kouici@insta.fr>
2026-04-01 09:13:30 +0200

    add params and function to send mail

--- system/serveur/usecase/process_candidat/index.py
+++ system/serveur/usecase/process_candidat/index.py
@@ -2,12 +2,17 @@ import resend
 
 resend.api_key = "re_KNzSW2NY_4DUhnNFD3qayPRs7BWwgZxtT"
 
-params = {
-    "from": "onboarding@resend.dev",
-    "to": "kouicicontact@yahoo.com",
-    "subject": "Test Resend Python",
-    "html": "<strong>Ça marche !</strong>"
-}
+def envoyer_mail(email_utilisateur, nom_utilisateur):
+    try:
+        params = {
+            "from": "onboarding@resend.dev",
+            "to": "kouicicontact@yahoo.com",
+            "subject": f"Confirmation pour {nom_utilisateur}",
+            "html": f"<strong>Bravo {nom_utilisateur} !</strong> Ton clic a bien été enregistré pour l'adresse {email_utilisateur}."
+        }
 
-email = resend.Emails.send(params)
-print(email)
+        r = resend.Emails.send(params)
+        print(f"Mail envoyé avec succès ! ID: {r['id']}")
+
+    except Exception as e:
+        print(f"Erreur lors de l'envoi : {e}")
